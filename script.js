ad7846d199cd620270d4b036c6b821d57a665e98
Krishna Kireeti Kesa <43212507+KrishKesa@users.noreply.github.com>
2021-08-05 19:04:21 -0400

    Javascript code added

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-script.js
+
+
+document.getElementById("submitform").addEventListener("click", function(){
+let name = document.getElementById('contactname').value;
+let email = document.getElementById('contactemail').value;
+let phone = document.getElementById('contactphone').value;
+let text = document.getElementById('contactmessage').value;
+
+    alert(`${name}, ${email}, ${phone}, ${text}`);    
+});
+
