63995d7c28c475fa083251260943c392c66669b4
Sudheer <sudhirsuri43@gmail.com>
2021-08-11 20:39:48 -0400

    jquery initial commit

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 
-
-document.getElementById("submitform").addEventListener("click", function(){
-let name = document.getElementById('contactname').value;
-let email = document.getElementById('contactemail').value;
-let phone = document.getElementById('contactphone').value;
-let text = document.getElementById('contactmessage').value;
-
-    alert(`${name}, ${email}, ${phone}, ${text}`);    
+$("document").ready(function(){
+    $("#submitform").click(function(){
+       let name = $("#contactname").val();
+       alert(name);
+    });
 });
+// document.getElementById("submitform").addEventListener("click", function(){
+// let name = document.getElementById('contactname').value;
+// let email = document.getElementById('contactemail').value;
+// let phone = document.getElementById('contactphone').value;
+// let text = document.getElementById('contactmessage').value;
+
+//     alert(`${name}, ${email}, ${phone}, ${text}`);    
+// });
 
